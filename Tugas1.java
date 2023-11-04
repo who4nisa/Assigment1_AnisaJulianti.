a2ffa4404875cdb30a1e847cb49e3d125d56239c
who4nisa <147330029+who4nisa@users.noreply.github.com>
2023-11-04 12:35:23 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,11 @@
+public class Tugas1 {
+    public static void main(String[] args)  {
+        for (int i = 1; i <= 100; i++) {
+            if (i <= 9) {
+                System.out.println(i);
+            } else {
+                System.out.println("Anisa Julianti");
+            }
+        }
+    }
+}
---
+++ Tugas4.java
@@ -0,0 +1,10 @@
+public class Tugas4 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Nilai dalam variabel array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
